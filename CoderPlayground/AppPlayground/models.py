f6c28de94e046ec03c93d9a541e45b09d039dcef
diegocastaldo <115197937+diegocastaldo@users.noreply.github.com>
2022-10-28 22:39:22 -0300

    Problemas con la app CoderPlayground resuelto! Guardamos...

--- CoderPlayground/AppPlayground/models.py
+++ CoderPlayground/AppPlayground/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class nobel(models.Model):
+    nomre=models.CharField(max_length=50)
+    año=models.IntegerField()
+    campo=models.CharField(max_length=20)
+
--- CoderPlayground/AppPlayground/urls.py
+++ CoderPlayground/AppPlayground/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from AppPlayground import views
 
 urlpatterns = [
-    path('admin/', site.urls),
+    path('', views.inicio),
 ]
